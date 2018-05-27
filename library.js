e03e2c2d04ec7046df7da69705580848a07f8671
Serhey  Shtanko <deathwings9@gmail.com>
2018-05-27 17:02:31 +0300

    Update library.js

--- library.js
+++ library.js
@@ -54,34 +54,88 @@
 		callback();
 	};
 
-	Vkontakte.getStrategy = function(strategies, callback) {
+	Vkontakte.getSettings = function(callback) {
+		if (Vkontakte.settings) {
+			return callback();
+		}
+
 		meta.settings.get('sso-vkontakte', function(err, settings) {
-			if (!err && settings.id && settings.secret) {
-				passport.use(new passportVK({
-					clientID: settings.id,
-					clientSecret: settings.secret,
-					callbackURL: nconf.get('url') + '/auth/vkontakte/callback'
-				}, function(accessToken, refreshToken, params, profile, done) {
-					Vkontakte.login(profile.id, profile.username, profile.displayName, params.email, profile.photos[0].value, function(err, User) {
-						var email = params.email;
+			Vkontakte.settings = settings;
+			callback();
+		});
+	};
+
+	Vkontakte.getStrategy = function(strategies, callback) {
+		if (!Vkontakte.settings) {
+			return Vkontakte.getSettings(function() {
+				Vkontakte.getStrategy(strategies, callback);
+			});
+		}
+
+		if (
+			Vkontakte.settings !== undefined
+			&& Vkontakte.settings.hasOwnProperty('app_id') && Vkontakte.settings.app_id
+			&& Vkontakte.settings.hasOwnProperty('secret') && Vkontakte.settings.secret
+		) {
+			passport.use(new passportVK({
+				clientID: Vkontakte.settings.app_id,
+				clientSecret: Vkontakte.settings.secret,
+				callbackURL: nconf.get('url') + '/auth/vkontakte/callback',
+				passReqToCallback: true,
+				profileFields: ['id', 'emails', 'name', 'displayName']
+			}, function(req, accessToken, refreshToken, profile, done) {
+				if (req.hasOwnProperty('user') && req.user.hasOwnProperty('uid') && req.user.uid > 0) {
+					// User is already logged-in, associate fb account with uid if account does not have an existing association
+					User.getUserField(req.user.uid, 'vkontakteid', function (err, fbid) {
 						if (err) {
 							return done(err);
 						}
-						done(null, User);
+
+						if (!vkontakteid || profile.id === vkontakteid) {
+							User.setUserField(req.user.uid, 'vkontakteid', profile.id);
+							db.setObjectField('vkontakteid:uid', profile.id, req.user.uid);
+							done(null, req.user);
+						} else {
+							done(new Error('[[error:sso-multiple-association]]'));
+						}
 					});
-				}));
+				} else {
+					var email;
+					if (profile._json.hasOwnProperty('email')) {
+						email = profile._json.email;
+					} else {
+						email = (profile.username ? profile.username : profile.id) + '@vk.com';
+					}
 
-				strategies.push({
-					name: 'vkontakte',
-					url: '/auth/vkontakte',
-					callbackURL: '/auth/vkontakte/callback',
-					icon: 'vk fa-vk',
-					scope: 'email'
-				});
-			}
+					Vkontakte.login(profile.id, profile.displayName, email, accessToken, refreshToken, profile, function(err, user) {
+						if (err) {
+							return done(err);
+						}
 
-			callback(null, strategies);
-		});
+						// Require collection of email
+						if (email.endsWith('@vk.com')) {
+							req.session.registration = req.session.registration || {};
+							req.session.registration.uid = user.uid;
+							req.session.registration.vkontakteid = profile.id;
+						}
+
+						authenticationController.onSuccessfulLogin(req, user.uid, function (err) {
+							done(err, !err ? user : null);
+						});
+					});
+				}
+			}));
+
+			strategies.push({
+				name: 'vkontakte',
+				url: '/auth/vkontakte',
+				callbackURL: '/auth/vkontakte/callback',
+				icon: constants.admin.icon,
+				scope: 'public_profile, email'
+			});
+		}
+
+		callback(null, strategies);
 	};
 
 	Vkontakte.appendUserHashWhitelist = function (data, callback) {
